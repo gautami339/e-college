f085bbebc749af8730bd1167ff935cebf638023b
gautami <gautami339@gmail.com>
2022-08-27 08:17:26 +0530

    Revert "Files basic changes"
    This reverts commit 1f229c09b9698d4bc1f3f73949d96bccf1a9ebe8.

--- E_college/settings.py
+++ E_college/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'e-college',
         'USER': 'postgres',
-        'PASSWORD': '1234',
+        'PASSWORD': '123',
         'HOST': 'localhost',
         'PORT': '5432',
     }
--- account/serializer.py
+++ account/serializer.py
@@ -1,4 +1,6 @@
 
+from email.policy import default
+from turtle import update
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from E_college.helper import AppHelpers
@@ -20,7 +22,7 @@ class UserNotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserNotification
         fields = ('user_id', 'message', 'types')
-
+    
     def get_types(self,obj):
         return AppHelpers.get_user_notification_type_name(obj.types)
 
@@ -36,15 +38,15 @@ class StatusupdateSerializer(serializers.ModelSerializer):
 class StudentMenuUpdateSerializer(serializers.ModelSerializer):
     status = serializers.IntegerField(write_only=True, default=MealStatus.ACCEPTED.value)
 
-    class Meta:
+    class Meta: 
         model = StudentMeal
         fields = ('status',)
 
 class StudentMenuTakenSerializer(serializers.ModelSerializer):
     status = serializers.IntegerField(write_only=True, default=MealStatus.TAKEN.value)
 
-    class Meta:
+    class Meta: 
         model = StudentMeal
         fields = ('status',)
-
+    
 
